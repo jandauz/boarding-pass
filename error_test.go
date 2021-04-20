06d29df8f7a5631f0577192a84671effe94c7c73
jandauz <jandauz@gmail.com>
2021-04-20 19:07:16 -0400

    Run gofmt (#6)

--- error_test.go
+++ error_test.go
@@ -3,13 +3,13 @@ package bcbp
 import "testing"
 
 func TestErrorType_String(t *testing.T) {
-    defer func() {
-        r := recover()
-        if r == nil {
-            t.Errorf("errorType.String() = nil: expected error")
-        }
-    }()
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("errorType.String() = nil: expected error")
+		}
+	}()
 
-    et := ErrorType("test")
-    _ = et.String()
+	et := ErrorType("test")
+	_ = et.String()
 }
--- example_test.go
+++ example_test.go
@@ -1,44 +1,44 @@
 package bcbp_test
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/jandauz/boarding-pass"
+	"github.com/jandauz/boarding-pass"
 )
 
 func ExampleFromStr() {
-    const s = "M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 326J001A0025 100"
-    b, err := bcbp.FromStr(s)
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println("Format Code:", b.FormatCode)
-    fmt.Println("NumberOfLegsEncoded:", b.NumberOfLegsEncoded)
-    fmt.Println("PassengerName:", b.PassengerName)
-    fmt.Println("ElectronicTicketIndicator:", b.ElectronicTicketIndicator)
-    fmt.Println("OperatingCarrierPNRCode:", b.Legs[0].OperatingCarrierPNRCode)
-    fmt.Println("FromCityAirportCode:", b.Legs[0].FromCityAirportCode)
-    fmt.Println("ToCityAirportCode:", b.Legs[0].ToCityAirportCode)
-    fmt.Println("OperatingCarrierDesignator:", b.Legs[0].OperatingCarrierDesignator)
-    fmt.Println("FlightNumber:", b.Legs[0].FlightNumber)
-    fmt.Println("DateOfFlight:", b.Legs[0].DateOfFlight)
-    fmt.Println("CompartmentCode:", b.Legs[0].CompartmentCode)
-    fmt.Println("SeatNumber:", b.Legs[0].SeatNumber)
-    fmt.Println("CheckInSequenceNumber:", b.Legs[0].CheckInSequenceNumber)
-    fmt.Println("PassengerStatus:", b.Legs[0].PassengerStatus)
-    // Output:
-    // Format Code: M
-    // NumberOfLegsEncoded: 1
-    // PassengerName: DESMARAIS/LUC
-    // ElectronicTicketIndicator: E
-    // OperatingCarrierPNRCode: ABC123
-    // FromCityAirportCode: YUL
-    // ToCityAirportCode: FRA
-    // OperatingCarrierDesignator: AC
-    // FlightNumber: 0834
-    // DateOfFlight: 2021-11-22
-    // CompartmentCode: J
-    // SeatNumber: 001A
-    // CheckInSequenceNumber: 0025
-    // PassengerStatus: 1
+	const s = "M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 326J001A0025 100"
+	b, err := bcbp.FromStr(s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Format Code:", b.FormatCode)
+	fmt.Println("NumberOfLegsEncoded:", b.NumberOfLegsEncoded)
+	fmt.Println("PassengerName:", b.PassengerName)
+	fmt.Println("ElectronicTicketIndicator:", b.ElectronicTicketIndicator)
+	fmt.Println("OperatingCarrierPNRCode:", b.Legs[0].OperatingCarrierPNRCode)
+	fmt.Println("FromCityAirportCode:", b.Legs[0].FromCityAirportCode)
+	fmt.Println("ToCityAirportCode:", b.Legs[0].ToCityAirportCode)
+	fmt.Println("OperatingCarrierDesignator:", b.Legs[0].OperatingCarrierDesignator)
+	fmt.Println("FlightNumber:", b.Legs[0].FlightNumber)
+	fmt.Println("DateOfFlight:", b.Legs[0].DateOfFlight)
+	fmt.Println("CompartmentCode:", b.Legs[0].CompartmentCode)
+	fmt.Println("SeatNumber:", b.Legs[0].SeatNumber)
+	fmt.Println("CheckInSequenceNumber:", b.Legs[0].CheckInSequenceNumber)
+	fmt.Println("PassengerStatus:", b.Legs[0].PassengerStatus)
+	// Output:
+	// Format Code: M
+	// NumberOfLegsEncoded: 1
+	// PassengerName: DESMARAIS/LUC
+	// ElectronicTicketIndicator: E
+	// OperatingCarrierPNRCode: ABC123
+	// FromCityAirportCode: YUL
+	// ToCityAirportCode: FRA
+	// OperatingCarrierDesignator: AC
+	// FlightNumber: 0834
+	// DateOfFlight: 2021-11-22
+	// CompartmentCode: J
+	// SeatNumber: 001A
+	// CheckInSequenceNumber: 0025
+	// PassengerStatus: 1
 }
